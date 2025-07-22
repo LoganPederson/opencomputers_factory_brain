50d0af7a24a848e7a94c92cab32e546f7e2cd69d
loganpederson <loganpederson@gmail.com>
2025-07-22 00:50:22 -0500

    add post to pastebin script

---
+++ scripts/pastebin_sync.py
@@ -0,0 +1,42 @@
+import requests
+
+# This script should post to PasteBin PasteBin
+# when we commit to git, using Git Hooks.
+
+
+
+# Get the contents of the factory_brain script
+def get_script_contents():
+    with open("../src/factory_brain.lua","r") as f:
+        script_contents = f.read()
+        return script_contents
+
+
+def post_to_pastebin():
+
+    api_dev_key="ryts-cmkSuk0-MFGi94daa4P7Tyd6jIB"
+    api_option="paste"
+    api_paste_code=get_script_contents()
+
+    data={
+        "api_dev_key":api_dev_key,
+        "api_option":api_option,
+        "api_paste_code":api_paste_code
+    }
+
+    r = requests.post(
+    url="https://pastebin.com/api/api_post.php",
+    data=data
+    )
+    
+
+    if r.status_code == 200:
+        return(r.text)
+    else:
+        return(f"ERROR: Status_Code ${r.status_code}")
+    #print(r.text)
+    #print(r.status_code)
+
+
+if __name__ == "__main__":
+    print(post_to_pastebin())
